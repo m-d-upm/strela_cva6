a41729e1f0ee9a27a7d57b4d7cd237439ffb03b1
m-d-upm <milos.dordevic@upm.es>
2025-10-03 11:55:35 +0200

    modified interrupt generation logic to trigger on event rising edge

--- rtl/axi_cgra_module/interrupt_controller.sv
+++ rtl/axi_cgra_module/interrupt_controller.sv
@@ -10,30 +10,37 @@ module interrupt_controller (
     output logic        int_line_o
 ); 
 
-    logic int_occured;
-    logic started_event;
+    logic int_occured_d;
+    logic started_event_d;
+    logic started_event_q;
+
+    logic event_started_prev_q;
+    logic event_done_prev_q;
 
     always_ff @(posedge clk_i or negedge rst_ni) begin
         if (~rst_ni) begin
+            event_started_prev_q <= 1'b0;
+            event_done_prev_q <= 1'b0;
+            started_event_q <= 1'b0;
             int_line_o <= 1'b0;
-            started_event <= 1'b0;
         end else begin
-            if (event_started_i) begin
-                started_event <= 1'b1;
+            event_started_prev_q <= event_started_i;
+            event_done_prev_q <= event_done_i;
+
+            if(started_event_d) begin
+                started_event_q <= 1'b1;
             end
 
-            if (int_occured) begin
+            if (int_occured_d) begin
                 int_line_o <= 1'b1;
             end
         end
     end
 
     always_comb begin
-       if (started_event && event_done_i) begin
-           int_occured = 1'b1; 
-       end else begin
-           int_occured = 1'b0;
-       end
+        // detect rising edge 0->1
+       started_event_d = event_started_i && !event_started_prev_q;
+       int_occured_d = event_done_i && !event_done_prev_q && started_event_q;
     end
 
 /*
